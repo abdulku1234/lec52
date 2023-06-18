7102dea961428b8313990b45028cf6d532d52ece
abdulku1234 <abdulkuddus@gmail.com>
2023-06-18 03:40:32 -0700

    jvujifdj

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+// single line comment
+//
+//multie line comment
+
+//lets take a js object for storing student information
+
+var student1_name = "Akil";
+var student_surname = 'pathaan';
+
+var student1 = {
+    //1 porperty:value
+    name:"Akil",
+    surname:'pathaan'
+    //2.
+    //JS
+
+
+};
+
+
+// How many way i can display the output of a JS code 
+// 1. way of to see the js code output
+
+// object.propertyname
+
+console.log("Hello "+student1.name);// is + is a concatination opreator 
---
+++ js/custom2.js
@@ -0,0 +1,9 @@
+
+
+const student4 = {
+    name:"Amaan",
+    surname:`Raeen`,
+    father_name:'Azad'
+};
+
+document.write('<div>'+student4.name+'</div>');
